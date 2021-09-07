cc5c15bca452ec9efebdb698e2bef478eecb9c86
StarDust85z <roadtomars2030@gmail.com>
2021-09-08 01:49:24 +0300

    add more sections

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,5 @@
+// hamburger & menu
+
 const hamburger = document.querySelector('.hamburger'),
     menu = document.querySelector('.menu'),
     menuClose = document.querySelector('.menu__close');
@@ -9,3 +11,10 @@ hamburger.addEventListener('click', () => {
 menuClose.addEventListener('click', () => {
     menu.classList.remove('active');
 });
+
+// progress bars
+
+const progress = document.querySelectorAll('.skills__item-status'),
+    progressBars = document.querySelectorAll('.skills__item-done');
+
+progress.forEach((item, i) => progressBars[i].style.width = item.textContent);
